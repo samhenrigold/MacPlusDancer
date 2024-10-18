e43a67e890e4aac7ba4acc51fad7c60382472e94
samhenrigold <samhenrigold@gmail.com>
2024-10-17 21:31:41 -0400

    Initial Commit

---
+++ MacPlusDancer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacPlusDancer
+//
+//  Created by Sam Gold on 2024-10-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacPlusDancer/MacPlusDancerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPlusDancerApp.swift
+//  MacPlusDancer
+//
+//  Created by Sam Gold on 2024-10-17.
+//
+
+import SwiftUI
+
+@main
+struct MacPlusDancerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
